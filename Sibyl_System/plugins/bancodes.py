f4a9e533dfc9f14945600465ff15aff573735333
Md Noor <75603153+Mdnoor786@users.noreply.github.com>
2022-05-08 23:56:12 +0530

    Update bancodes.py

--- Sibyl_System/plugins/bancodes.py
+++ Sibyl_System/plugins/bancodes.py
@@ -1,21 +1,59 @@
-help_plus = """ Here is Help for **Bancodes** :-
-**•** `{SLx00}` - **SCAMMER.**
-**•** `{SLx01}` - **SPAM ADDING MEMBER.**
-**•** `{SLx03}` - **ABUSE SPAM.**
-**•** `{SLx04}` - **NSFW SPAMMER.**
-**•** `{SLx06}` - **IMPERSONATION.**
-**•** `{SLx07}` - **MD/BTC SCAM.**
-**•** `{SLx08}` - **ADDING SPAMBOTS.**
-**•** `{SLx10}` - **ILLEGAL.**
-**•** `{SLx11}` - **PHISHING.**
-**•** `{SLx12}` - **FRAUD PROMOTION  [ANY KIND].**
-**•** `{SLx13}` - **CYBER THREATENING / CYBER BULLY.**
-**•** `{SLx14}` - **CHILD ABUSE.**
-**•** `{SLx15}` - **BAN EVASION.**
-**•** `{SLx16}` - **SPAMBOT.**
-**•** `{SLx17}` - **RAID INITIALIZOR.** 
-**•** `{SLx18}` - **RAID PARTICIPANT.**
-**•** `{SLx19}` - **KRIMINALANT.**
-**•** `{SLx20}` - **RAID/SPAM INFLAMMER.**
+help_plus = """ SaVior Ban codes / what can be banned?
+
+p.s -> ban codes are special codes which represents the reason of the fed ban ...if your reason comes under the following , You can appeal
+
+**•**{SVx00} - Scam 
+**•**{SVx01} - Spamming without permit in public groups
+**•**{SVx02} - too much abuse / toxicity
+**•**{SVx03} -  Spam (PM)
+ **•**{SVX04} - Spamming 18+ content
+**•**{SVx05} - Without credits/ copyright
+**•**{SVx06} - Clonning / Impersonation
+**•**{SVx07} - Bitcoin scam /escrow scam
+**•**{SVx08} - Adding spam bots
+**•**{SVx09} - Abuse of administration rights
+**•**{SVx10} - Doing something illegal (India)
+**•**{SVx11} - Phishing fraud 
+**•**{SVx12} -  Fraud (untrusted)
+**•**{SVx13} - Scrapping members 
+**•**{SVx14.1} -  Blackmail (girls)
+**•**{SVx14.2} - Blackmail (boy)
+**•**{SVx15} - Leaking private stuff (pic , name , location , contact)
+**•**{SVx16} - 18+ content (child)
+**•**{SVx17} - 18+ content ( affected person)
+**•**{SVx18} - Spambot (18+)
+**•**{SVx19} -  Raiders 
+**•**{SVx20} -  Fake federation and bans
+**•**{SVx21} -  Religious abuse
+**•**{SVx22} - National abuse
+**•**{SVx23} Gbans and Gmute without any reason
+**•**{SVx24} Misuse of owner rights 
+**•**{SVx25} money fraud
+**•**{SVx26} copy without permission
+**•**{SVx27} Fraud in cross promotion
+**•**{SVx28} Misuse of sudo rights
+**•**{SVx29} misuse of co owner rights
+**•**{SVx30} fake giveaways 
+**•**{SVx31} Fake proofs
+**•**{SVx32} Dming girls and asking for photo / friendship / nudes
+**•**{SVx33} Clonning federations
+**•**{SVx34} suppression
+**•**{SVx35} call without permission
+**•**{SVx36} mass calling from fake numbers
+**•**{SVx37} Spreading terrorist propoganda
+**•**{SVx38} Mass report without any reason
+**•**{SVx39} Sending malwares in place of legit hack /pack
+**•**{SVx40} Hacking
+**•**{SVx41} Fraud promotion
+**•**{SVx42} Cyber bully
+**•**{SVx43} Child abuse
+**•**{SVx44} Raid initialisor
+**•**{SVx45} Agreeing to Raid
+**•**{SVx46} Raid participant
+**•**{SVx47} spamming violent
+**•**{SVx48} Discrimination based on colour
+**•**{SVx49} Discrimination based on race
+**•**{SVx50} Discrimination based on gender
+**•**{SVx51} Hacking Using Userbot/Plugin
 """
 __plugin_name__ = "bancodes"
